f5b6817f560541a2ca852968a5b6586d396927a2
Mihael <Mihael@DESKTOP-I40Q5NG.lan>
2023-04-24 19:01:56 -0400

    Added name to GUI file

--- BaseballBatterStatsApp/src/main/java/com/csd2522/baseballbatterstatsapp/BatterGUIApp.java
+++ BaseballBatterStatsApp/src/main/java/com/csd2522/baseballbatterstatsapp/BatterGUIApp.java
@@ -1,5 +1,5 @@
 /*
-Author: Batter Team Daniel Cronauer, Next person
+Author: Batter Team Daniel Cronauer, Michael Mowad
 Date: 4/21/2023 added to project
 File: Console.java
 Purpose: Driver App, starts main window for App
