e8d20f486eb83010941442ec2f9381baafbc0138
Daniel Cronauer <dcronaue@gmail.com>
2023-04-26 21:26:08 -0400

    major changes got player add to handle new and update existing players

--- BaseballBatterStatsApp/src/main/java/com/csd2522/Batter/Batter.java
+++ BaseballBatterStatsApp/src/main/java/com/csd2522/Batter/Batter.java
@@ -7,7 +7,78 @@ package com.csd2522.Batter;
 /**
  *
  * @author Locke
+ * Daniel Cronauer created basic functionality for Batter class so I can pass the Batter around 4/26/2023
  */
 public class Batter {
+    //define instance variables
+    private int PlayerID;
+    private String firstName;
+    private String lastName;
+    private String team;
     
+    public Batter(int PlayerID, String firstName, String lastName, String team)
+    {
+        this.PlayerID = PlayerID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.team = team;
+        
+        
+    }
+
+    /**
+     * @return the PlayerID
+     */
+    public int getPlayerID() {
+        return PlayerID;
+    }
+
+    /**
+     * @param PlayerID the PlayerID to set
+     */
+    public void setPlayerID(int PlayerID) {
+        this.PlayerID = PlayerID;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the team
+     */
+    public String getTeam() {
+        return team;
+    }
+
+    /**
+     * @param team the team to set
+     */
+    public void setTeam(String team) {
+        this.team = team;
+    }
 }
--- BaseballBatterStatsApp/src/main/java/com/csd2522/DB/BatterDB.java
+++ BaseballBatterStatsApp/src/main/java/com/csd2522/DB/BatterDB.java
@@ -9,6 +9,10 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.lang.StringBuilder;
+import java.util.Collections;
+import java.util.TreeMap;
+import com.csd2522.Batter.Batter;
+
 /**
  *
  * @author Daniel Cronauer 4/22/2023 Carried over DB from chapter 19 and used for this project
@@ -77,7 +81,46 @@ public class BatterDB {
             System.out.println(e);
         }    
     }
-    // This method will link to database and attach players to HashMap, then return HashMap
+    // return Batter object with player id, player last name, and player first name, and player team based on player id passed to this function DC 4/26/2023
+    public Batter returnPlayer(int playerID)
+    {
+        Batter player = null;
+           try(PreparedStatement ps = connection.prepareStatement("SELECT Player_id, Player_last_name, Player_first_name, Player_team_id FROM Players "
+                 + "WHERE Player_id =? "))            
+        {
+            
+            //tell us what the ? paramter will take value of teamId passed to this method this should filter players to ones assigned to team DC 4/26/2023
+            ps.setInt(1,playerID);
+            
+            // create ResultSet object DC 4/26/2023
+            ResultSet rs = ps.executeQuery();
+            
+            
+            rs.next();
+            // get variables to build Batter object 4/26/2023
+            int PlayerID = rs.getInt(1);
+            String lastName = rs.getString(2);
+            String firstName = rs.getString(3);
+            String team = rs.getString(4);
+            
+            //make batter object
+            player = new Batter(PlayerID,firstName, lastName, team);
+            
+            
+            rs.close();
+            
+            
+        }
+        catch (SQLException e)
+        {       
+            System.out.println("Error in query filling players");
+            System.out.println(e);
+        } 
+        return player;
+    }
+    
+    
+    // This method will link to database and attach teams to HashMap, then return HashMap
     // filled with string for team game and game ID as value pair. DC 4/25/2023
     public HashMap<String,Integer> getTeams() 
     {
@@ -118,6 +161,56 @@ public class BatterDB {
         return games;
     }
     
+     // This method will link to database and attach players to TreeMap, then return treeMap
+    // filled with string player full name as key and player id as value. DC 4/26/2023
+    public TreeMap<String,Integer> getPlayers(String teamID) 
+    {
+//        try(PreparedStatement ps = connection.prepareStatement("SELECT Game_team_one_id, Game_team_two_id, Game_win_id, Game_team_one_score, Game_team_two_score, Game_date "
+//                 + "FROM Games WHERE Game_id =?"))  
+        // create hash map filled with game information as key and game id as value DC 4/26/2023
+        TreeMap<String, Integer> players = new TreeMap<>();
+        
+         try(PreparedStatement ps = connection.prepareStatement("SELECT Player_id, Player_last_name, Player_first_name, Player_team_id FROM Players "
+                 + "WHERE Player_team_id =? ORDER BY 2"))            
+        {
+            
+            //tell us what the ? paramter will take value of teamId passed to this method this should filter players to ones assigned to team DC 4/26/2023
+            ps.setString(1,teamID);
+            
+            // create ResultSet object DC 4/25/2023
+            ResultSet rs = ps.executeQuery();
+            
+            
+            //as long as there is a result in result set we will continue going DC 4/26/2023
+            while (rs.next())
+            {
+                // want to get variables for each column from select statement to build players treeMap up DC 4/26/2023
+                int playerID = rs.getInt(1);
+                String playerLast = rs.getString(2);
+                String playerFirst = rs.getString(3);
+                String teamText = rs.getString(4);
+                
+                // build string for key DC 4/26/2023
+                
+                String key = playerLast + ", " + playerFirst;
+                
+                // add new element to TreeMap DC 4/26/2023
+                players.put(key, playerID);
+                
+            }
+            
+            rs.close();
+        }
+        catch (SQLException e)
+        {       
+            System.out.println("Error in query filling players");
+            System.out.println(e);
+        }    
+         
+        //return filled treeMap DC 4/25/2023
+        return players;
+    }
+    
     public static ArrayList<String> getTeamIDs(){
         ArrayList<String> teamIDList = new ArrayList<>();
         
@@ -136,7 +229,8 @@ public class BatterDB {
             System.out.println("Error in query filling teams");
             System.out.println(e);
         }    
-        
+        // sort so teams list is nicely sorted DC 4/26/2023.
+        Collections.sort(teamIDList);
         return teamIDList;
     }    
     // this method will start as test, print game information to console, then when complete, I will 
